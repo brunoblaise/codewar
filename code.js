d05b1a089fd90f6ad4d4e4b0c4d43a6ecce60175
Bruno Blaise <brunoblaise94@gmail.com>
2022-08-25 21:59:27 +0200

    codewars

---
+++ code.js
@@ -0,0 +1,19 @@
+function hero(bullets, dragons) {
+  //Get Coding!
+  return bullets === dragons * 2 ? true : false;
+
+  //   if (bullets === dragons * 2) return true;
+
+  //   return false;
+}
+
+function countSheeps(arrayOfSheep) {
+  // TODO May the force be with you
+  let count = 0;
+
+  arrayOfSheep.forEach((sheeps) => {
+    // console.log(sheeps);
+    if ( sheeps === true)  return  count += 1
+  });
+}
+
